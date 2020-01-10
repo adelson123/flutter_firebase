331ae5a37c19c38405e8b963954a958db2054daf
aluno <aluno@LAB0X-P0X.labs.riolargo>
2020-01-10 10:26:06 -0300

    App em flutter com firebase

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+
+import 'package:app_firebase/tela_cadastro.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Cadastro',
+      theme: ThemeData(
+        primaryColor: Colors.purple,
+      ),
+      home: TelaCadastro(),
+    );
+  }
+}
---
+++ lib/tela_cadastro.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+class TelaCadastro extends StatefulWidget {
+  @override
+  _TelaCadastroState createState() => _TelaCadastroState();
+}
+
+class _TelaCadastroState extends State<TelaCadastro> {
+  @override
+  Widget build(BuildContext context) {
+    final pularLinha = Divider();
+
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        centerTitle: true,
+        title: Text(
+          "Formulário",
+          textAlign: TextAlign.center,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextFormField(
+                  keyboardType: TextInputType.text,
+                  style: TextStyle(color: Colors.black, fontSize: 17),
+                  decoration: InputDecoration(
+                    labelText: "Nome",
+                    labelStyle: TextStyle(color: Colors.black),
+                  ),
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  style: TextStyle(color: Colors.black, fontSize: 17),
+                  decoration: InputDecoration(
+                    labelText: "CPF",
+                    labelStyle: TextStyle(color: Colors.black),
+                  ),
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(color: Colors.black, fontSize: 17),
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    labelStyle: TextStyle(color: Colors.black),
+                  ),
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.text,
+                  obscureText: true,
+                  style: TextStyle(color: Colors.black, fontSize: 17),
+                  decoration: InputDecoration(
+                    labelText: "Senha",
+                    labelStyle: TextStyle(color: Colors.black),
+                  ),
+                ),
+                pularLinha,
+                ButtonTheme(
+                  height: 50.0,
+                  buttonColor: Colors.white,
+                  child: RaisedButton(
+                    onPressed: () => {},
+                    child: Text("Cadastrar", style: TextStyle(fontSize: 17)),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+           children: <Widget>[
+      DrawerHeader(
+        child: Text('Drawer Header'),
+        decoration: BoxDecoration(
+          color: Colors.amber,
+        ),
+      ),
+      ListTile(
+        title: Text('Perfil'),
+        onTap: () {
+          // Update the state of the app.
+          // ...
+        },
+      ),
+      ListTile(
+        title: Text('Usuários'),
+        onTap: () {
+          // Update the state of the app.
+          // ...
+        },
+      ),
+       ListTile(
+        title: Text('Sair'),
+        onTap: () {
+          // Update the state of the app.
+          // ...
+        },
+      ),
+    ],
+        ),
+      ),
+    );
+  }
+}
